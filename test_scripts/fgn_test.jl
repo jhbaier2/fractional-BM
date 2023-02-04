13de716424d3e4e3fab9ee7ef877994e77e65076
Jacob Baier <jake.baier789@gmail.com>
2023-02-04 16:20:02 -0600

    .

--- test_scripts/fgn_test.jl
+++ test_scripts/fgn_test.jl
@@ -1,21 +1,33 @@
-using Random, Plots, GSL, Distributions, LinearAlgebra
+using Random, Plots, FFTW, Distributions, LinearAlgebra
 
-function autocov(δt, H)
-    if δt<0
-        δt = abs(δt)
-    elseif δt==0
-        return 1.
-    else
-        γₖ = 1/2*((δt+1)^(2H) + abs(δt-1)^(2H) - (2δt)^(2H))
-        return γₖ
+function autocov(s, t, H)
+    if s > t
+        s, t = t, s
     end
+    G = 2H
+    γₕ = 1/2*(t^G + s^G - abs(t-s)^G)
+    return γₕ
 end
 
+function autocov(n, H)
+    G = 2H
+    γₕ = 1/2*((n+1)^G + abs(n-1)^G - (2n)^G)
+    return γₕ
+end
 
-function fgn(H, T, n, m; seed=nothing, kwargs...)
+function Λ(H,N)
+    M = 2N - 2
+    C = zeros(Float64, (1,M))
+    C[1:N] .= autocov.(0:N-1, H)
+    C[N+1:M] .= reverse(C[2:N-1], dims=2)
+    return sqrt.(real(fft(C)))
+end
+
+function fgn(H, T, N; npath=1, seed=nothing, kwargs...)
+    # fractional gaussian noise
     rng = MersenneTwister(seed)
     δt = T/n
-    Gn = randn(rng, (m,n))
+    Gn = randn(rng, (N,npath))
     if H==0.5
         fGn = Gn
     else
@@ -25,8 +37,9 @@ function fgn(H, T, n, m; seed=nothing, kwargs...)
 end
 
 begin
-    m = 10
-    n = 1
+    q = 10
+    N = 2^q + 1
+    npath = 1
     seed = 1234
     H = 0.3
 
