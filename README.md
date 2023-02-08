b1fd5b500435bd7dec4fa2075937a48f5cc8b47f
Jake Engelen <jake.engelen@avmltd.com>
2023-02-08 15:55:55 -0500

    add tests with summary stats and random numbers from matlab

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # fractional-BM
 Main source for fBM sim:
-
 Georgiy Shevchenko, "Fractional Brownian Motion in a Nutshell"
+
+We Want to find: a relationship between parameters of rough fractional stochastic volatility (see Volatility is rough - Gatheral) and Garch
+
+How: fix rough fractional stochastic volatility parameters, use quasi maximum likelihood estimator to find garch params
+
+Question to ask: 1. How well/poorly does it fit?
+                 2. How complicated in some sort of rejection threshold?
+                 3.Are there situations where rfsv can't be modelled by garch (analytic approximation for something whose vol is rfsv)?
+
+Steps: 1. fgn paths -> rough fractional stochastic volatility paths
+       2. quasi maximum likelihood with garch
+       3. Further Steps? Document findings, paper etc
+
+    
--- test_scripts/fgn_test.jl
+++ test_scripts/fgn_test.jl
@@ -1,4 +1,20 @@
-using Random, Plots, FFTW, Distributions, LinearAlgebra
+using Random, Plots, FFTW, Distributions, LinearAlgebra, Test, Statistics
+
+
+function autocov(n, H)
+    G = 2H
+    γₕ = 1/2*((n+1)^G + abs(n-1)^G - 2n^G)
+    return γₕ
+end
+
+function Λ(H,N)\
+    M = 2N-2
+    C = zeros(Float64, (1,M))
+    C[1:N] .= autocov.(0:N-1, H)
+    C[N+1:M] .= reverse(C[2:N-1])
+    Λ = sqrt.(real(fft(C)))
+    return  Λ
+end
 
 function autocov(s, t, H)
     if s > t
@@ -7,20 +23,6 @@ function autocov(s, t, H)
     G = 2H
     γₕ = 1/2*(t^G + s^G - abs(t-s)^G)
     return γₕ
-end
-
-function autocov(n, H)
-    G = 2H
-    γₕ = 1/2*((n+1)^G + abs(n-1)^G - (2n)^G)
-    return γₕ
-end
-
-function Λ(H,N)
-    M = 2N - 2
-    C = zeros(Float64, (1,M))
-    C[1:N] .= autocov.(0:N-1, H)
-    C[N+1:M] .= reverse(C[2:N-1])
-    return sqrt.(real(fft(C)))
 end
 
 function fgn(H, T, N; npath=1, seed=nothing, kwargs...)
@@ -36,11 +38,64 @@ function fgn(H, T, N; npath=1, seed=nothing, kwargs...)
     return fGn
 end
 
+function fgn(Λ, NT)
+
+    rng = MersenneTwister(0) #matlab default rng
+    M = size(Λ, 2)
+    rands = randn(NT, M)
+    b = ifft(rands)
+    a = broadcast(*, b, Λ)
+    fGn = real(fft(a,2))[:, 1: Int(M/2)]
+    return fGn  
+end
+
+begin
+    q = 2
+    N = 2^q + 1
+    H = 0.3
+    lam = Λ(H,N)
+    fgn(lam, 20)
+end
+
+
+
+
+#=
+-------------------TESTS--------------- -> put in test file
+#test fgn
+begin 
+    rands = [[-1.3239,	0.7503,	-0.7004,	0.8401,	-1.0852,	0.0909,	-0.4381,	-1.784,]
+    [-0.2794,	-0.5158,	-0.8662,	-1.2097,	1.1274,	1.0755,	-0.7616,	1.3512,]
+    [0.7854,	1.3949,	-0.2259,	1.8403,	-0.7558,	-0.111,	0.2349,	0.9666,]
+    [-0.9421,	-1.3855,	-1.2795,	-0.6421,	-0.5374,	-1.2935,	2.2342,	-0.1436,]
+    [0.6595,	1.7095,	0.7379,	0.7423,	-0.8658,	0.6412,	0.7097,	0.2791]]
+    # use rands in the fgn nfunction for variable "rands"
+
+    q = 2
+    N = 2^q + 1
+    H = 0.3
+    lam = Λ(H,N)
+    fgn(lam, 20)
+
+    # matlab outputs std(fGn) = 1.2170, mean = 0.0074, max = 
+
+
+
+#Test autocov 
+begin
+    n=10
+    H=.05
+    ac = autocov(n, H)
+    @test ac ≈ 0.5837 atol=0.001
+end
+
+#Test  Λ 
 begin
     q = 10
     N = 2^q + 1
-    npath = 1
-    seed = 1234
     H = 0.3
-    Λ(H,N)
+    lam = Λ(H,N)
+    @test std(lam) ≈ 0.1984 atol=0.001 
+    #0.1984 is a matlab check from Shevchenko
 end
+=#
